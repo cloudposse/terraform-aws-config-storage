c907d5318179c6e4dceb76d6fc53fe9d7f74b344
Matt Calhoun <matt@mattcalhoun.com>
2020-12-14 11:15:59 -0500

    add policy

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ module "storage" {
   version = "0.14.0"
   count   = module.this.enabled ? 1 : 0
 
+  policy                                 = data.aws_iam_policy_document.aws_config_bucket_policy[0].json
   lifecycle_prefix                       = var.lifecycle_prefix
   lifecycle_tags                         = var.lifecycle_tags
   force_destroy                          = var.force_destroy
